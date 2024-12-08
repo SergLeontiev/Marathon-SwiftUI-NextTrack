800f9879f3c0bfdb072c0cca88aff4641edc109b
Sergey Leontiev <translatertrans@gmail.com>
2024-12-08 12:49:19 +0100

    Minor fix

--- Marathon-SwiftUI-NextTrack/ContentView.swift
+++ Marathon-SwiftUI-NextTrack/ContentView.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State var buttonTapped: Bool = false
+    private let imageName = "play.fill"
     
     var body: some View {
         Button {
+            guard !buttonTapped else { return }
             withAnimation(.bouncy) {
                 buttonTapped = true
             } completion: {
@@ -21,23 +23,23 @@ struct ContentView: View {
             GeometryReader { proxy in
                 let size = proxy.size.width / 2
                 HStack(alignment: .center, spacing: 0) {
-                    Image(systemName: "play.fill")
+                    Image(systemName: imageName)
                         .resizable()
                         .scaledToFit()
                         .frame(width: buttonTapped ? size : 0)
                         .opacity(buttonTapped ? 1 : 0)
-                    Image(systemName: "play.fill")
+                    Image(systemName: imageName)
                         .resizable()
                         .scaledToFit()
                         .frame(width: size)
-                    Image(systemName: "play.fill")
+                    Image(systemName: imageName)
                         .resizable()
                         .scaledToFit()
                         .frame(width: buttonTapped ? 1 : size)
                         .opacity(buttonTapped ? 0 : 1)
-                }.frame(maxHeight: .infinity)
-            }.frame(width: 80, alignment: .center)
-        }
+                }.frame(maxHeight: .infinity, alignment: .center)
+            }
+        }.frame(maxWidth: 80)
     }
 }
 
